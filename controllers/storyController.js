2f47f0d6e6d8d0a157e16ea72378510fe3b63821
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-06-18 21:32:32 +0100

    implemented pagination

--- controllers/storyController.js
+++ controllers/storyController.js
@@ -8,15 +8,52 @@ const ForbiddenError = require('../errors/forbidden');
 const UnauthenticatedError = require('../errors/unauthenticated');
 
 exports.getAllStories = catchAsync(async (req, res, next) => {
-  const features = new APIFeatures(Story.find({ status: 'public' }), req.query)
-    .filter()
-    .sort()
-    .limitFields()
-    .paginate();
+  // filtering
+  const queryObj = { ...req.query };
+  const excludedFields = ['page', 'sort', 'limit', 'fields'];
+  excludedFields.forEach((item) => delete queryObj[item]);
 
-  const stories = await features.query;
+  // advanced filering
+  let queryStr = JSON.stringify(queryObj);
+  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/, (match) => `$${match}`);
 
-  res.status(StatusCodes.OK).send(stories);
+  let query = Story.find(JSON.parse(queryStr));
+
+  // sorting
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  // limiting fields
+  if (req.query.fields) {
+    const fields = req.query.fields.split(',').join(' ');
+    query = query.select(fields);
+  } else {
+    query = query.select('-__v');
+  }
+
+  // pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 6;
+  const skip = (page - 1) * limit;
+
+  const total = await Story.find({ status: 'public' }).countDocuments();
+  query = query.skip(skip).limit(limit);
+
+  const numberOfPages = Math.ceil(total / limit);
+
+  let stories = await query;
+  stories = stories.filter((item) => item.status === 'public');
+
+  res.status(StatusCodes.OK).json({
+    totalStories: total,
+    currentPage: page,
+    numberOfPages,
+    stories,
+  });
 });
 
 exports.getStoryById = catchAsync(async (req, res, next) => {
