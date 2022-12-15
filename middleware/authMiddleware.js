010d5d65180c2061b52d87a43193332847c48557
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-15 10:43:39 +0100

    created authMiddleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,55 @@
+const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
+
+const catchAsync = require('../utils/catchAsync');
+const UnauthenticatedError = require('../errors/unauthenticated');
+const ForbiddenError = require('../errors/forbidden');
+
+exports.protect = catchAsync(async (req, res, next) => {
+  let token;
+  const authHeader = req.headers.authorization;
+
+  if (authHeader && authHeader.startsWith('Bearer')) {
+    token = authHeader.split(' ')[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
+  }
+
+  if (!token) {
+    return next(
+      new UnauthenticatedError(
+        'You are not logged in! Please log in to get access'
+      )
+    );
+  }
+
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  const currentUser = await User.findById(decoded.id);
+
+  if (!currentUser) {
+    return next(
+      new UnauthenticatedError(
+        'The user belonging to this token does no longer exist'
+      )
+    );
+  }
+
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new UnauthenticatedError(
+        'User recently changed password! Please log in again'
+      )
+    );
+  }
+
+  req.user = currentUser;
+  res.locals.user = currentUser;
+  next();
+});
+
+exports.verifyUser = (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    return next();
+  }
+  return next(new ForbiddenError('You are not authorized'));
+};
