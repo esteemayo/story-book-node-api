2eaec2a506d6f00a58a8129ef04ec38f29f5db22
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-16 16:56:42 +0100

    modified

--- controllers/authController.js
+++ controllers/authController.js
@@ -46,48 +46,6 @@ exports.login = catchAsync(async (req, res, next) => {
   createSendToken(user, StatusCodes.OK, req, res);
 });
 
-exports.protect = catchAsync(async (req, res, next) => {
-  let token;
-  const authHeader = req.headers.authorization;
-
-  if (authHeader && authHeader.startsWith('Bearer')) {
-    token = authHeader.split(' ')[1];
-  } else if (req.cookies.jwt) {
-    token = req.cookies.jwt;
-  }
-
-  if (!token) {
-    return next(
-      new UnauthenticatedError(
-        'You are not logged in! Please log in to get access'
-      )
-    );
-  }
-
-  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-  const currentUser = await User.findById(decoded.id);
-
-  if (!currentUser) {
-    return next(
-      new UnauthenticatedError(
-        'The user belonging to this token does no longer exist'
-      )
-    );
-  }
-
-  if (currentUser.changedPasswordAfter(decoded.iat)) {
-    return next(
-      new UnauthenticatedError(
-        'User recently changed password! Please log in again'
-      )
-    );
-  }
-
-  req.user = currentUser;
-  res.locals.user = currentUser;
-  next();
-});
-
 exports.isLoggedIn = async (req, res, next) => {
   if (req.cookies.jwt) {
     try {
