649891bebc0a1e4df864d83bb4526d1196e82894
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-12-16 17:32:18 +0100

    added db,js

---
+++ config/db.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const devEnv = process.env.NODE_ENV !== 'production';
+const { DATABASE, DATABASE_LOCAL, DATABASE_PASSWORD } = process.env;
+
+// db local
+const dbLocal = DATABASE_LOCAL;
+
+// atlas mongo uri
+const mongoURI = DATABASE.replace(
+  '<PASSWORD>',
+  DATABASE_PASSWORD
+);
+
+const db = devEnv ? dbLocal : mongoURI;
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(db);
+    console.log(`Connected to MongoDB → ${conn.connection.port}`.gray.bold);
+  } catch (err) {
+    throw err;
+  }
+};
+
+module.exports = connectDB;
