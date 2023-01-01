33c04cd72ce1065acf50a6863f3c65b55f332e01
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-01 02:22:22 +0100

    fix: change to es6 module

--- errors/badRequest.js
+++ errors/badRequest.js
@@ -1,5 +1,5 @@
-const { StatusCodes } = require('http-status-codes');
-const AppError = require('./appError');
+import { StatusCodes } from 'http-status-codes';
+import AppError from './appError.js';
 
 class BadRequestError extends AppError {
   constructor(message) {
@@ -10,4 +10,4 @@ class BadRequestError extends AppError {
   }
 }
 
-module.exports = BadRequestError;
+export default BadRequestError;
