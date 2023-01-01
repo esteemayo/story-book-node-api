64aa3d5d133ee6be02ebb92776c1273ca5f52693
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-01 02:31:47 +0100

    fix: change to es6 module

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,15 +1,15 @@
-const factory = require('./handlerFactory');
-const Comment = require('../models/Comment');
+import Comment from '../models/Comment.js';
+import * as factory from './handlerFactory.js';
 
-exports.sendStoryUserIds = (req, res, next) => {
+export const sendStoryUserIds = (req, res, next) => {
   if (!req.body.story) req.body.story = req.params.storyId;
   if (!req.body.user) req.body.user = req.user.id;
 
   next();
 };
 
-exports.getAllComments = factory.getAll(Comment);
-exports.getComment = factory.getOne(Comment);
-exports.createComment = factory.createOne(Comment);
-exports.updateComment = factory.updateOne(Comment);
-exports.deleteComment = factory.deleteOne(Comment);
+export const getAllComments = factory.getAll(Comment);
+export const getComment = factory.getOne(Comment);
+export const createComment = factory.createOne(Comment);
+export const updateComment = factory.updateOne(Comment);
+export const deleteComment = factory.deleteOne(Comment);
