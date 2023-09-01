f1cad4c07e646c0c2d59bcab0e00a94b33cddeb7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-09-01 04:31:25 +0100

    fix: modify

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,7 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 
-import catchAsync from '../utils/catchAsync.js';
 import APIFeatures from '../utils/apiFeatures.js';
+import catchAsync from '../utils/catchAsync.js';
 import NotFoundError from '../errors/notFound.js';
 
 export const getAll = (Model) =>
